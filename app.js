acb7c4f539fee02fc398e2bd2498518bb229d7b1
doritobandito <michael.white308@gmail.com>
2016-08-23 21:42:14 +0800

    Removed gabe.json & express & slack-client

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var env = require("./env.js")
-var express = require('express');
-var app = express();
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
 var token = process.env.API_TOKEN;
-console.log(token);
+var gabe = null;
 
-var rtm = new RtmClient(token, {logLevel: 'debug'});
+var rtm = new RtmClient(token);
 rtm.start();
 
-/*rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('Message:', message);
-});*/
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+	console.log('Message:', message);
+	if (message.text.includes("gabe")) {
+		rtm.sendMessage('Hello', message.channel);
+	}
 });
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+	gabe = rtmStartData;
+	console.log(gabe.self.name);
+});
+
+function gabeMentioned(message) {
+	
+}
